b0442077144b5a23d31edd6f3c670ea1c3741224
Neilsilenso <96867641+Neilsilenso@users.noreply.github.com>
2022-09-21 09:48:27 +0530

    Update Q18.dart

--- Assignment_M1_Collection1_File_Count_22/Q18.dart
+++ Assignment_M1_Collection1_File_Count_22/Q18.dart
@@ -1,9 +1,5 @@
 // Write a Dart program to test an list is empty or not.
 void main() {
   var list = [3];
-  if (list.length == 0) {
-    print('List is empty.');
-  } else {
-    print('List is not empty.');
-  }
+  print(list.isEmpty);
 }
