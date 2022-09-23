f0751c65327608349f9468327fc25e6cafb95d3c
Neilsilenso <96867641+Neilsilenso@users.noreply.github.com>
2022-09-23 23:18:05 +0530

    Update Q9.dart

--- Assignment_M1_Isolates_File_Count_10/Q9.dart
+++ Assignment_M1_Isolates_File_Count_10/Q9.dart
@@ -1,1 +1,27 @@
 //Include isolate concept in a calculator operation using OOPS
+
+import 'dart:isolate';
+
+class Calculator{
+   addition(var val1,var val2){
+    print("Sum of $val1 + $val2 is ${val1+val2}");
+  }
+  subtraction(var val1,var val2){
+    print("Difference of $val1 - $val2 is ${val1-val2}");
+  }
+  division(var val1,var val2){
+    print("Division of $val1 / $val2 is ${val1~/val2}");
+       }
+    multiplication(var val1,var val2){
+      print("Product of $val1 * $val2 is ${val1*val2}");
+    }
+}
+void main(){
+  Calculator();
+  Calculator obj=Calculator();
+  Isolate.spawn(obj.addition(2, 4),true);
+  Isolate.spawn(obj.subtraction(2, 4) , true);
+  Isolate.spawn(obj.division(2, 4),true);
+  Isolate.spawn(obj.multiplication(2, 4),true);
+
+}
