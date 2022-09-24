2bc09a2e5bdb1cdc39a925bc31f9fc4ee86cd6e7
Neilsilenso <96867641+Neilsilenso@users.noreply.github.com>
2022-09-24 15:59:52 +0530

    Update Q10.dart

--- Assignment_M1_Isolates_File_Count_10/Q10.dart
+++ Assignment_M1_Isolates_File_Count_10/Q10.dart
@@ -6,3 +6,29 @@ squared. A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to
 BMI applies to most adults 18-65 years.]
  */
 
+import 'dart:io';
+
+void main() async{
+  double bmi;
+  print("Enter the Height in Meter");
+  double m=double.parse(stdin.readLineSync()!);
+  print("Enter the Weight in KiloGram");
+  int kg=int.parse(stdin.readLineSync()!);
+  print("Enter your age");
+  int age=int.parse(stdin.readLineSync()!);
+  if(age>=18 && age<=65){
+    print("BMI is calculating");
+    await Future.delayed(Duration(seconds: 2),(){
+
+    });
+  bmi=kg~/m*m;
+  if(bmi>=25.0){
+    print(bmi);
+    print("You are overweight");
+  }else if(bmi>=18.5 && bmi<=24.9){
+    print("Yor are in a healthy condition");
+  }
+}else{
+    print("BMI is calculated only between adults 18-65");
+  }
+}
