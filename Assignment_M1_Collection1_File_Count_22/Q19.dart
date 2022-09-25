ea79f4a0a14eb60dbf42f60b23c190e7f8fe13eb
Neilsilenso <96867641+Neilsilenso@users.noreply.github.com>
2022-09-25 11:51:03 +0530

    Update Q19.dart

--- Assignment_M1_Collection1_File_Count_22/Q19.dart
+++ Assignment_M1_Collection1_File_Count_22/Q19.dart
@@ -1,5 +1,7 @@
 // Write a Dart program to trim the capacity of an list the current list size.
 void main(){
-  var list=[2,4,6,8,10,12,14,16,18,20];
-  list.
+  var list=[11,2,3,5,6,7,8,19];
+  print(list);
+  list.removeRange(0,4);
+  print (list);
 }
